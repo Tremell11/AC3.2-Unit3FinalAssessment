5f7b3e242814f9e50abed8229c7648daba4eb718
Margaret E. Ikeda <margaretei@gmail.com>
2016-11-10 11:29:13 -0500

    setting up project files and storyboard

---
+++ VictoriaAlbert/VictoriaAlbert/ApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  ApiManager.swift
+//  VictoriaAlbert
+//
+//  Created by Margaret Ikeda on 11/10/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import Foundation
---
+++ VictoriaAlbert/VictoriaAlbert/VictoriaObject.swift
@@ -0,0 +1,9 @@
+//
+//  VictoriaObject.swift
+//  VictoriaAlbert
+//
+//  Created by Margaret Ikeda on 11/10/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import Foundation
