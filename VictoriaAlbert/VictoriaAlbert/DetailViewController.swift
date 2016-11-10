7f8fd06e0640e76176f3f41cb2d55289d3296175
Mira Estil <imz.estil@gmail.com>
2016-11-10 18:53:04 -0500

    Updates

---
+++ VictoriaAlbert/VictoriaAlbert/DetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.swift
+//  VictoriaAlbert
+//
+//  Created by Ilmira Estil on 11/10/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    let imgEndpoint = "http://media.vam.ac.uk/media/thira/collection_images/2006AM/2006AM6763_jpg_o.jpg"
+    var detailMuseumField: [Museum]?
+    @IBOutlet weak var detailImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*
+        if detailMuseumField.count > 0 {
+            APIRequestManager.manager.getData(endPoint: ) { (data: Data?) in
+                if let validData = data,
+                    let image = UIImage(data: validData) {
+                    DispatchQueue.main.async {
+                        acvc.briefMovieImage.image = image
+                        acvc.setNeedsLayout()
+                        
+                    }
+                }
+            }
+        }
+
+        detailImage.image = detailMuseumField
+    
+ */
+ }
+
+    
+ 
+        
+
+   
+
+}
